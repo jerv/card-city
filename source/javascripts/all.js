32248b7b649c911f2150e186010b8d36f6824189
Jeremy Venegas <chief@jeremys-mbp.attlocal.net>
2015-09-14 12:10:56 -0700

    nav improvements

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,5 +5,9 @@ $(function(){
   	$(this).toggleClass('active');
   	$('nav ul').toggleClass('active');
   });
+  $('nav ul').click(function(){ 
+  	$(this).toggleClass('active');
+  	$('#toggle').toggleClass('active');
+  });
 });
 
